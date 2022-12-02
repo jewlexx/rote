e57eebd1abcdfb8ff59b6b91895290081635015c
Juliette Cordor <grassojames5@gmail.com>
2022-12-02 20:03:18 +1100

    added egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use tracing::Level;
+
+#[macro_use]
+extern crate tracing;
+
 fn main() {
-    println!("Hello, world!");
+    use tracing_subscriber::fmt::format::FmtSpan;
+
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::CLOSE | FmtSpan::NEW)
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    let native_options = eframe::NativeOptions::default();
 }
