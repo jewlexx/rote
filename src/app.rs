b3be48119e6534d5c0d8a28a1c45f7180211430f
Juliette Cordor <grassojames5@gmail.com>
2022-12-05 20:06:23 +1100

    fix

--- src/app.rs
+++ src/app.rs
@@ -48,7 +48,9 @@ impl eframe::App for EditorApp {
         let window_size = frame.info().window_info.size;
 
         egui::Area::new("main_editor").show(ctx, |ui| {
-            ui.text_edit_multiline(contents);
+            ui.set_min_height(100.0);
+            ui.set_max_height(100.0);
+            ui.add_sized(ui.available_size(), egui::TextEdit::multiline(contents));
         });
     }
 }
