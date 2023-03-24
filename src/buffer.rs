16c2d6925d26035e981186fb4dceac674fb4c04e
Juliette Cordor <juliette@jewelexx.com>
2023-03-24 21:58:16 +1100

    added buffer impl

---
+++ src/buffer.rs
@@ -0,0 +1,52 @@
+// TODO: Custom serialize with for contentsbuffer
+
+#[derive(Debug, Clone)]
+pub struct ContentsBuffer {
+    pub contents: String,
+
+    pub edited: bool,
+    pub mutable: bool,
+}
+
+impl Default for ContentsBuffer {
+    fn default() -> Self {
+        Self {
+            contents: String::new(),
+            edited: false,
+            mutable: true,
+        }
+    }
+}
+
+impl ContentsBuffer {
+    pub fn get_contents_mut(&mut self) -> &mut String {
+        &mut self.contents
+    }
+
+    pub fn edited(&self) -> bool {
+        self.edited
+    }
+
+    pub fn set_edited(&mut self, edited: bool) {
+        self.edited = edited;
+    }
+}
+
+impl egui::TextBuffer for ContentsBuffer {
+    fn is_mutable(&self) -> bool {
+        self.contents.is_mutable()
+    }
+
+    fn as_str(&self) -> &str {
+        self.contents.as_str()
+    }
+
+    fn insert_text(&mut self, text: &str, char_index: usize) -> usize {
+        self.edited = true;
+        self.contents.insert_text(text, char_index)
+    }
+
+    fn delete_char_range(&mut self, char_range: std::ops::Range<usize>) {
+        self.contents.delete_char_range(char_range);
+    }
+}
