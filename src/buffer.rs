4f226610ef5e2d650b29cab480cd57425dca7165
Juliette Cordor <juliette@jewelexx.com>
2023-03-24 23:55:10 +1100

    fixes

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,7 @@
 // TODO: Custom serialize with for contentsbuffer
 
+use egui::TextBuffer;
+
 #[derive(Debug, Clone)]
 pub struct ContentsBuffer {
     pub contents: String,
@@ -45,12 +47,28 @@ impl egui::TextBuffer for ContentsBuffer {
         self.contents.as_str()
     }
 
+    fn clear(&mut self) {
+        self.set_edited(true);
+        self.contents.clear();
+    }
+
+    fn take(&mut self) -> String {
+        self.set_edited(true);
+        self.contents.take()
+    }
+
     fn insert_text(&mut self, text: &str, char_index: usize) -> usize {
         self.set_edited(true);
         self.contents.insert_text(text, char_index)
     }
 
     fn delete_char_range(&mut self, char_range: std::ops::Range<usize>) {
+        self.set_edited(true);
         self.contents.delete_char_range(char_range);
+    }
+
+    fn replace(&mut self, text: &str) {
+        self.set_edited(true);
+        self.contents.replace(text);
     }
 }
