fd20b5d07818f11192bb61d9002365f87356a41f
Juliette Cordor <juliette@jewelexx.com>
2023-03-24 23:08:43 +1100

    added shortcuts

---
+++ src/shortcuts.rs
@@ -0,0 +1,24 @@
+use egui::{Button, Context, Key, KeyboardShortcut, Modifiers};
+use strum::EnumIter;
+
+#[derive(Debug, Copy, Clone, EnumIter)]
+pub enum Shortcut {
+    Open,
+    Close,
+    Quit,
+}
+
+impl Shortcut {
+    pub fn get_details(&self) -> (&'static str, KeyboardShortcut) {
+        match self {
+            Shortcut::Open => ("Open", KeyboardShortcut::new(Modifiers::CTRL, Key::O)),
+            Shortcut::Close => ("Close", KeyboardShortcut::new(Modifiers::CTRL, Key::W)),
+            Shortcut::Quit => ("Quit", KeyboardShortcut::new(Modifiers::CTRL, Key::Q)),
+        }
+    }
+
+    pub fn into_button(self, ctx: &Context) -> Button {
+        let (title, shortcut) = self.get_details();
+        Button::new(title).shortcut_text(ctx.format_shortcut(&shortcut))
+    }
+}
