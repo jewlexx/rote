5dea0a611a00b43dbb8893926e33c8431945e688
Juliette Cordor <juliette@jewelexx.com>
2023-03-25 18:32:16 +1100

    improved diff buffer

--- src/diff.rs
+++ src/diff.rs
@@ -1,19 +1,59 @@
-use difference::Changeset;
 use parking_lot::Mutex;
 
-pub static DIFF_BUFFER: Mutex<Vec<Changeset>> = Mutex::new(Vec::new());
-
-pub fn add_change(old: &str, new: &str) {
-    DIFF_BUFFER.lock().push(Changeset::new(old, new, " "));
+#[derive(Debug)]
+pub enum Diff {
+    Add(String, usize),
+    Remove(String, usize),
 }
 
-pub fn apply_change(current: &str) {
-    let latest_diff = DIFF_BUFFER.lock().pop();
+impl Diff {
+    pub fn invert(self) -> Self {
+        match self {
+            Diff::Add(a, b) => Diff::Remove(a, b),
+            Diff::Remove(a, b) => Diff::Add(a, b),
+        }
+    }
+}
 
-    if let Some(diff) = latest_diff {
-        dbg!(diff.diffs);
-        // diff.apply(current)
+pub static DIFF_BUFFER: DiffBuffer = DiffBuffer::new();
+
+#[derive(Debug)]
+pub struct DiffBuffer {
+    diffs: Mutex<Vec<Diff>>,
+}
+
+impl DiffBuffer {
+    pub const fn new() -> Self {
+        Self {
+            diffs: Mutex::new(Vec::new()),
+        }
     }
 
-    // todo!()
+    pub fn add(&self, text: String, position: usize) {
+        self.add_change(Diff::Add(text, position))
+    }
+
+    pub fn delete(&self, text: String, position: usize) {
+        self.add_change(Diff::Remove(text, position))
+    }
+
+    pub fn add_change(&self, diff: Diff) {
+        dbg!(&diff);
+        self.diffs.lock().push(diff);
+    }
 }
+
+// pub fn add_change(old: &str, new: &str) {
+//     DIFF_BUFFER.lock().push(Changeset::new(old, new, " "));
+// }
+
+// pub fn apply_change(current: &str) {
+//     let latest_diff = DIFF_BUFFER.lock().pop();
+
+//     if let Some(diff) = latest_diff {
+//         dbg!(diff.diffs);
+//         // diff.apply(current)
+//     }
+
+//     // todo!()
+// }
